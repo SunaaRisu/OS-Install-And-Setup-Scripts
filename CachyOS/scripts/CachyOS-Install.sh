6f7e9502d17babc598072ae95caa32ad3c89f012
Sunaa <105628373+SunaaRisu@users.noreply.github.com>
2026-09-12 13:22:22 +0200

    refactoring

--- CachyOS/scripts/CachyOS-Install.sh
+++ CachyOS/scripts/CachyOS-Install.sh
@@ -1,30 +1,41 @@
 #!/bin/bash
 
 # Connect to the Internet
+echo "Checking internet connection"
 ping -c 1 cachy.sunaarisu.de
 if [ $? -eq 0 ]; then 
   echo "Connected"
 else 
-  echo "Try Wifi connection"
+  printf "Try Wifi connection:\n\n"
   read -r -p "SSID: " wifissid
   read -r -p "Password: " wifipwd
   iwctl --passphrase $wifipwd station wlan0 connect $wifissid
   ping -c 1 archlinux.org
   if [ $? -eq 0 ]; then
-    echo "Connected"
+    printf "\nConnected"
   else
-    echo "Connection Error"
+    printf "\nConnection Error"
     exit 1
   fi
 fi
+clear -x
+
+# Get needed variables and settings
+# Disk selection
+printf "Which disk should be partitioned?\n\n"
+lsblk
+printf "\n\n"
+read -r -p "/dev/" partDisk
+clear -x
+# Encrypt disk
+read -r -p "Should the root disk be encrypted? [y/N] " encrypt
+clear -x
+
 
 # Update the system clock
 timedatectl
 
 # Partition the disks
-lsblk
-echo "Which disk should be partitioned?"
-read -r -p "/dev/" partDisk
 cfdisk /dev/${partDisk}
 
 # Format the Partitions
@@ -35,8 +46,7 @@ fi
 mkswap /dev/${partDisk}2
 mkfs.fat -F 32 /dev/${partDisk}1
 
-read -r -p "Encrypt the disk? [y/N] " response
-if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
+if [[ "$encrypt" =~ ^([yY][eE][sS]|[yY])$ ]]
 then
   cryptsetup luksFormat /dev/${partDisk}3
   cryptsetup open /dev/${partDisk}3 cryptroot
@@ -54,10 +64,41 @@ mount --mkdir /dev/${partDisk}1 /mnt/boot
 swapon /dev/${partDisk}2
 
 # Install essential packages
-pacstrap -K /mnt base linux linux-firmware sof-firmware base-devel grub efibootmgr nvim networkmanager man btop fastfetch git tree sudo
+pacman -Syy
+pacstrap -K /mnt base base-devel linux linux-firmware btrfs-progs limine efibootmgr nvim networkmanager bash-completio man btop fastfetch git tree sudo
 
 # Generate fstab
 genfstab /mnt > /mnt/etc/fstab
+
+
+
+
+
+
+
+
+
+
+exit 0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # Changing root
 arch-chroot /mnt /bin/bash <<END
